07ca7ec1718e89a54e9f0a2c5aaaa744bcbb58a0
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-27 21:47:31 +0530

    max_value

--- Program.cs
+++ Program.cs
@@ -6,31 +6,33 @@ namespace GeniricMethod
 {
     public class Program
     {
-        public void Calculation<T>(T a, T b, T c)
+        public void Calculation<T>(T a, T b, T c,T d)
         {
             a = a;
             b = b;
             c = c;
+            d = d;
             List<T> list = new List<T>();
             list.Add(a);
             list.Add(b);
             list.Add(c);
+            list.Add(d);
             var Max_value = list.Max();
             Console.WriteLine(Max_value);
-
+            
         }
         static void Main(string[] args)
         {
             Program program = new Program();
             Console.WriteLine("Max Value of int :");
             List<int> use_int = new List<int>();
-            program.Calculation(1, 2, 50);
+            program.Calculation(1, 2, 50,100);
             Console.WriteLine("\nMax value of flote :");
             List<float> list1 = new List<float>();
-            program.Calculation(1.1, 2.2, 3.3);
+            program.Calculation(1.1, 2.2, 3.3,4.4);
             Console.WriteLine("\nMax value of string :");
             List<string> list = new List<string>();
-            program.Calculation("sam", "naveen", "saranya");
+            program.Calculation("sam", "naveen", "saranya","mythil");
 
         }
     }
