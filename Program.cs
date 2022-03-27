ef9ab0ac4c5830df0ca94841a645b524be14fd5f
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-27 16:11:47 +0530

    int compare

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GeniricMethod
+{
+    public class Programr
+    {
+        public void compare(int a, int b, int c)
+        {
+            int res = a.CompareTo(b);
+            if (res == 1)
+            {
+                int res2 = a.CompareTo(c);
+                if (res2 == 1)
+                {
+                    Console.WriteLine("First value {0} is greater then",+a);
+                }
+                else
+                {
+                    Console.WriteLine("Third value {0} is greater than",+c);
+                }
+            }
+            else
+            {
+                int res3 = b.CompareTo(c);
+                if (res3 == 1)
+                {
+                    Console.WriteLine("Second value {0} is greater than ",+b);
+                }
+                else
+                {
+                    Console.WriteLine("Third value {0} is grater than",+c);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Programr programr = new Programr();
+            programr.compare(5,10,15);
+        }
+    }
+}
